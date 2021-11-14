41429ba6b118a30b106e9c0832821fdbea5405d4
Shreesh Pandey <shvspandey@gmail.com>
2021-11-14 21:32:55 +0530

    Test Case for Invoke method Use Case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,68 +7,176 @@ namespace MoodAnalyzerMSTest
     [TestClass]
     public class UnitTest1
     {
-        MoodAnalyzerFactory factory = null;
-
-        [TestMethod]
-        public void SetUp()
+        [TestClass]
+        public class UnitTest1  //here performed the unit testing for class
         {
-            factory = new MoodAnalyzerProblem.MoodAnalyzerFactory();
-        }
 
-        [TestMethod]
-        public void AnalyseHappyMood()
-        {
-            // Test Case 1
-            //Arrange
-            string message = null;
-            string expected = "happy";
+            MoodAnalyzerFactory factory = null;
 
-            //Act
-            MoodAnalyzer modeAnalyzer = new MoodAnalyzer(message);
-            string actual = modeAnalyzer.analyseMood();
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        [TestMethod]
-        public void getCustomNullException()
-        {
-            //Arrange
-            string expected = "Message Should Not Be Null";
-            ModeAnalyzer modeAnalyzer = new ModeAnalyzer(null);
-
-            try
+            [TestInitialize]
+            public void SetUp()
             {
+                factory = new MoodAnalyzerFactory();
+            }
+
+            [TestMethod]
+            public void AnalyseHappyMood()
+            {
+                // Test Case 1
+                //Arrange
+                string message = null;
+                string expected = "happy";
+
                 //Act
-                string actual = modeAnalyzer.AnalyzeMood();
+                MoodAnalyzer MoodAnalyzer = new MoodAnalyzer(message);
+                string actual = MoodAnalyzer.AnalyzeMood();
+
+                //Assert
+                Assert.AreEqual(expected, actual);
 
             }
-            catch (CustomException ex)
+
+            [TestMethod]
+            public void getCustomNullException()
             {
+                //Arrange
+                string expected = "Message Should Not Be Null";
+                MoodAnalyzer MoodAnalyzer = new MoodAnalyzer(null);
 
-                //Assart
-                Assert.AreEqual(expected, ex.Message);
+                try
+                {
+                    //Act
+                    string actual = MoodAnalyzer.AnalyzeMood();
+
+                }
+                catch (CustomException ex)
+                {
+
+                    //Assart
+                    Assert.AreEqual(expected, ex.Message);
+                }
+
             }
 
+            /// <summary>
+            /// UC4
+            /// Reflections the using default constructor.
+            /// </summary>
+            /// <exception cref="System.Exception"></exception>
+            [TestMethod]
+            public void ReflectionUsingDefaultConstructor()
+            {
+                MoodAnalyzer expected = new MoodAnalyzer();
+                object obj = null;
+                try
+                {
+                    obj = factory.createMoodAnalyzerObject("MoodAnalyzarProblem.MoodAnalyzer", "MoodAnalyzer");
+
+                }
+                catch (CustomException ex1)
+                {
+                    throw new Exception(ex1.Message);
+                }
+                obj.Equals(expected);
+            }
+
+            /// <summary>
+            /// UC5
+            /// Gets the mood Analyzer using parametre constructor.
+            /// TC5.1
+            /// </summary>
+            /// <exception cref="System.Exception"></exception>
+            [TestMethod]
+            public void getMoodAnalyzerUsingParametreConstructor()
+            {
+                string message = "I am in happy mood";
+                MoodAnalyzer expected = new MoodAnalyzer(message);
+                object actual = null;
+                try
+                {
+                    MoodAnalyzerFactory factory = new MoodAnalyzerFactory();
+                    //act
+                    actual = factory.CreateMoodAnalyzerParameterObject("MoodAnalyzer", "MoodAnalyzer", message);
+
+                }
+                catch (CustomException exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+                actual.Equals(expected);
+            }
+
+            /// <summary>
+            /// Gets the mood Analyzer using parametre constructor1.
+            /// UC5 
+            /// TC5.2 Pass wrong class name
+            /// </summary>
+            /// <exception cref="System.Exception"></exception>
+            [TestMethod]
+            public void getMoodAnalyzerUsingParametreConstructorPassWrongClassName()
+            {
+                string message = "I am in happy mood";
+                MoodAnalyzer expected = new MoodAnalyzer(message);
+                object actual = null;
+                try
+                {
+                    MoodAnalyzerFactory factory = new MoodAnalyzerFactory();
+                    //act
+                    actual = factory.CreateMoodAnalyzerParameterObject("MoodAnalyzer123", "MoodAnalyzer", message);
+
+                }
+                catch (CustomException exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+                actual.Equals(expected);
+            }
+
+            /// <summary>
+            /// Gets the mood Analyzer using parametre constructor pass wrong constructor.
+            ///  UC5  TC5.5
+            /// </summary>
+            /// <exception cref="System.Exception"></exception>
+            [TestMethod]
+            public void getMoodAnalyzerUsingParametreConstructorPassWrongConstructor()
+            {
+                string message = "I am in happy mood";
+                MoodAnalyzer expected = new MoodAnalyzer(message);
+                object actual = null;
+                try
+                {
+                    MoodAnalyzerFactory factory = new MoodAnalyzerFactory();
+                    //act
+                    actual = factory.CreateMoodAnalyzerParameterObject("MoodAnalyzer", "MoodAnalyzerconstrcutor", message);
+
+                }
+                catch (CustomException exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+                actual.Equals(expected);
+            }
+
+            [TestMethod]
+            public void testMoodAlalyserUisngInvokeMethod()
+            {
+                string message = "Happy";
+                string methodName = "AnalyseMood";
+                string expected = "Happy";
+                string actual = " ";
+                try
+                {
+                    MoodAnalyzerFactory factory = new MoodAnalyzerFactory();
+
+                    actual = factory.InvokeAnalyzerMethod(message, methodName);
+                }
+                catch (CustomException exception)
+                {
+                    throw new Exception(exception.Message);
+
+                }
+                Assert.AreEqual(expected, actual);
+
+            }
         }
-
-        [TestMethod]
-        public void ReflectionUsingDefaultConstructor()
-        {
-            ModeAnalyzer expected = new ModeAnalyzer();
-            object obj = null;
-            try
-            {
-                obj = factory.createMoodAnalyserObject("MoodAnalyzarProblem.ModeAnalyzer", "ModeAnalyzer");
-
-            }
-            catch (CustomException ex1)
-            {
-                throw new Exception(ex1.Message);
-            }
-            obj.Equals(expected);
-        }
-    }
 }
