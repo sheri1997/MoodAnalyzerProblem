c5dc89fa3a62e0ad762507aae2645e850390fe54
Shreesh Pandey <shvspandey@gmail.com>
2021-11-14 19:07:44 +0530

    Displaying SAD Message for Test Case1.1

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        public string analyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
